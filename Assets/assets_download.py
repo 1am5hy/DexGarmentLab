79b6ffdc52bc3fd7063c07f32d11bba6ee615756
wayrise <1849089774@qq.com>
2025-08-01 10:31:08 +0800

    update assets & garment parameter markdown

--- Assets/assets_download.py
+++ Assets/assets_download.py
@@ -2,7 +2,7 @@ from huggingface_hub import snapshot_download
 
 snapshot_download(
     repo_id="wayrise/DexGarmentLab",
-    allow_patterns=["Garment.zip", "Robots.zip", "Scene.zip", "LeapMotion.zip"],
+    allow_patterns=["Garment.zip", "Robots.zip", "Scene.zip", "LeapMotion.zip", "Human.zip"],
     local_dir=".",
     repo_type="dataset",
     resume_download=True,
